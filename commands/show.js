0c0ee9e268e587def1c8b80db354fde5fdbb2ce6
Wheelz108 <51121319+Wheelz108@users.noreply.github.com>
2021-07-23 16:38:10 +1000

    Update .gitignore and commands/show.js

--- commands/show.js
+++ commands/show.js
@@ -2,7 +2,10 @@ module.exports = {
 	name: 'show',
 	description: 'Fetches info about a show',
 	execute(message, args) {
-		message.channel.send(`Here's what I found for ${args}`);
-		
-	},
+		if (!args.length) {
+			return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+		} else if (args.length) {
+			return message.channel.send(`Here's what I found for \`${args}\` `);
+		}
+	}
 };
